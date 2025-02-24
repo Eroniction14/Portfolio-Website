9d9f87f4b50a79195be28f05158012a8015a1bcb
Eroniction14 <53464628+Eroniction14@users.noreply.github.com>
2025-02-24 12:46:44 -0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,11 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-   base: "/Portfolio-Website/",
+    build: {
+        chunkSizeWarningLimit: 100000000
+    },
+    base: "/Portfolio-Website/",
 })
